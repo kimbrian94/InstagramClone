7834869a1eaece9f6caca45b2e4cd0642e134dd3
Brian Kim <kim.brian94@gmail.com>
2020-07-28 17:16:23 -0400

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Brian Kim on 2020-07-28.
+//  Copyright © 2020 Brian Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
